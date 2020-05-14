b2281d0f3ca03f2d341a2a661116896a65cf8bd0
chickenproductions <chickenproductions@users.noreply.github.com>
2020-05-13 21:38:12 -0700

    Moved the light a little

--- Assets/Camera/VoronoiSplitScreen.cs
+++ Assets/Camera/VoronoiSplitScreen.cs
@@ -33,18 +33,12 @@ public class VoronoiSplitScreen : MonoBehaviour
 
     [Header("Material Setttings")]
     public Material mat;
-   public RenderTexture camRT1;
+    public RenderTexture camRT1;
     public RenderTexture camRT2;
+
     void Awake()
     {
         mat.SetFloat("Rotation", 0);
-        //ChangeRes();
-        //camRT1 = new RenderTexture(Screen.width, Screen.height, 16);
-        //camRT2 = new RenderTexture(Screen.width, Screen.height, 16);
-        //mat.SetTexture("Cam1", camRT1);
-        //mat.SetTexture("Cam2", camRT2);
-        //Cam1.targetTexture = camRT1;
-        //Cam2.targetTexture = camRT2;
         player1 = GameObject.Find("1");
         if(GameController.playerCount == GameController.PlayerCount.Multiplayer)
         {
@@ -58,12 +52,8 @@ public class VoronoiSplitScreen : MonoBehaviour
     
     void Update()
     {
-
-        
-
-        // Debug.Log(player1.transform.position);
-        // gets distance
-        float distance = Vector2.Distance(player1.transform.position, player2.transform.position);
+            // gets distance
+            float distance = Vector2.Distance(player1.transform.position, player2.transform.position);
             //checks distance
            
             //subtracts distance for lerping for when falloff should start
@@ -73,8 +63,7 @@ public class VoronoiSplitScreen : MonoBehaviour
             Rotation = Mathf.Atan2(player1.transform.position.y - player2.transform.position.y, player1.transform.position.x - player2.transform.position.x) * Mathf.Rad2Deg * -1;
 
             // Material property settings
-            mat.SetFloat("Rotation", Rotation);
-            //Vector2 playernormalized = new Vector2((player1.position.x + player2.position.x) / 2, (player1.position.y + player2.position.y) / 2);      
+            mat.SetFloat("Rotation", Rotation);    
 
             //gets/sets rotational offset of Player1
 
@@ -94,9 +83,6 @@ public class VoronoiSplitScreen : MonoBehaviour
             Vector3 P2SplitPos = new Vector3(player2.transform.position.x + fx2, player2.transform.position.y - fy2, -10);
             Vector3 ConvergedPos = new Vector3((player1.transform.position.x + player2.transform.position.x) / 2, (player1.transform.position.y + player2.transform.position.y) / 2, -10);
 
-        // TEMPORARY REMOVE LATER,THIS IS JUST FOR NOW TO MAKE SURE THE SHOOTY THINGY WORKS
-        //Cam3.transform.position = ConvergedPos;
-
         // Blends the two transforms so it doesnt feel trash    
         Cam1.transform.position = Vector3.Lerp(ConvergedPos, P1SplitPos, distRange);
         Cam2.transform.position = Vector3.Lerp(ConvergedPos, P2SplitPos, distRange);
@@ -108,10 +94,7 @@ public class VoronoiSplitScreen : MonoBehaviour
         if (camRT1.width != Screen.width || camRT1.height != Screen.height)
         {
             ChangeRes();
-        }
-        
-     
-        
+        }    
     }
   
     //resizes render to textures, this can be hopefully passed throughw henever resolution or aspect ratio changes occur
@@ -125,10 +108,4 @@ public class VoronoiSplitScreen : MonoBehaviour
         Cam1.targetTexture = camRT1;
         Cam2.targetTexture = camRT2;
     }
-    //public static void LookPlayers()
-    //{
-       // player1 = GameObject.Find("Player");
-       // player2 = GameObject.Find("Player2");
-      //  Debug.Log("it worked kindof");
-    //}
 }
