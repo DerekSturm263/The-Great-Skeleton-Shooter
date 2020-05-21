7e3c8b07900c6e23774f0c3df4e98c8d20878369
Derek Sturm <dereksturm263@gmail.com>
2020-05-21 11:11:54 -0700

    Fixed the issues that were abundant.

--- Assets/Resources/Scripts/Main/Entity/Ally/AllyActions.cs
+++ Assets/Resources/Scripts/Main/Entity/Ally/AllyActions.cs
@@ -31,7 +31,7 @@ public class AllyActions : EntityActions
         pos.y = target.transform.position.y;
         pos.z = 0f;
 
-        armPivot.transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(pos.y - armPivot.transform.position.y, pos.x - armPivot.transform.position.x));
+        armPivot[0].transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(pos.y - armPivot[0].transform.position.y, pos.x - armPivot[0].transform.position.x));
     }
 
     // Called twice every second to try and fire at the player
@@ -40,11 +40,11 @@ public class AllyActions : EntityActions
         if (!(data as AllyData).isLockedOn)
             return;
 
-        GameObject newBullet = Instantiate(bullet, armPivot.transform.position + armPivot.transform.right * 0.5f, Quaternion.identity);
+        GameObject newBullet = Instantiate(bullet, armPivot[0].transform.position + armPivot[0].transform.right * 0.5f, Quaternion.identity);
 
-        newBullet.GetComponent<Rigidbody2D>().AddForce(armPivot.transform.right * bulletForce);
+        newBullet.GetComponent<Rigidbody2D>().AddForce(armPivot[0].transform.right * bulletForce);
         newBullet.GetComponent<Bullet>().SetBulletOwner(1);
-        newBullet.transform.rotation = armPivot.transform.rotation;
+        newBullet.transform.rotation = armPivot[0].transform.rotation;
 
         Destroy(newBullet, bulletLife);
     }
