48314d3a18063143bd95b6441ee366fa278c5e9a
chickenproductions <chickenproductions@users.noreply.github.com>
2020-05-21 17:51:37 -0700

    Added stuff for testing and stuff, mainly pushing to pull though.

---
+++ Assets/TestArmAim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D.Animation;
+public class TestArmAim : MonoBehaviour
+{
+    public Transform armb;
+    public Transform armb2;
+    public Animator anim;
+    public SpriteSkin sp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+           armb.transform.localEulerAngles += new Vector3(0, 0, 5);
+            armb2.transform.localEulerAngles += new Vector3(0, 0, 5);
+        }        
+    }
+}
