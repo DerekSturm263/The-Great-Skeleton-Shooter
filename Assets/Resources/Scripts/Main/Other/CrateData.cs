7e35fc4069d4554d1a266da51d7c7547dcc12a99
Derek Sturm <dereksturm263@gmail.com>
2020-06-01 14:58:31 -0700

    Improved most of the particle effects. Added some particles where there weren't any before.

--- Assets/Resources/Scripts/Main/Other/CrateData.cs
+++ Assets/Resources/Scripts/Main/Other/CrateData.cs
@@ -22,7 +22,11 @@ public class CrateData : MonoBehaviour
     private void SpawnItem(GameObject g)
     {
         GameObject newItem = Instantiate(g, gameObject.transform.position, gameObject.transform.rotation);
-        newItem.GetComponent<Rigidbody2D>().SetRotation(Random.Range(0f, 360f));
-        newItem.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-5f, 5f), 5f);
+
+        if (newItem.GetComponent<Rigidbody2D>() != null)
+        {
+            newItem.GetComponent<Rigidbody2D>().SetRotation(Random.Range(0f, 360f));
+            newItem.GetComponent<Rigidbody2D>().velocity = new Vector2(Random.Range(-5f, 5f), 5f);
+        }
     }
 }
